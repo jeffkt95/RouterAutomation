79a2aec0ef96e257820ce7bb48aa99b3b22112ac
Jeff Lyons <jeffkt95@gmail.com>
2018-06-21 07:22:42 -0400

    Program arguments

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import requests
+import sys
 
 from ParentalControlMessage import ParentalControlMessage
 
 def main():
+    numArgs = len(sys.argv)
+    if (numArgs < 3):
+        print("Not enough arguments. Pass in router password and 'block' or 'unblock'")
+        return 1
+    password = sys.argv[1]
+    blockOrUnblock = sys.argv[2]
+    
+    #Use testBlockStatus on my devices
+    testBlockStatus = '0'
+    blockStatus = '0'
+    if (blockOrUnblock == "block"):
+        #testBlockStatus = '1'
+        blockStatus = '1'
+    elif (blockOrUnblock == "unblock"):
+        #testBlockStatus = '0'
+        blockStatus = '0'
+    else:
+        print("Argument failure. Second argument should be 'block' or 'unblock'")
+        
+    
+
     print("Hello world.")
     routerUrl = "http://192.168.1.1/"
-    response = requests.get(routerUrl, auth=('admin', 'FakePassword'))
+    response = requests.get(routerUrl, auth=('admin', password))
     print(response.status_code)
     
     message = ParentalControlMessage()
@@ -13,18 +35,16 @@ def main():
     payload = message.getPayload()
     
     message.setNumDevices('5')
-    #message.setDevice0BlockStatus('Jeffreys-iPhone', '1')
-    #message.setDevice1BlockStatus('Jeffreys-iPad', '1')
-    message.setDeviceBlockStatus('0', 'Jeffreys-iPhone', '0')
-    message.setDeviceBlockStatus('1', 'Jeffreys-iPad', '0')
-    message.setDeviceBlockStatus('2', 'LyonsDesktopXPS-2', '0')
-    message.setDeviceBlockStatus('3', 'Chriss-iPhone', '0')
-    message.setDeviceBlockStatus('4', 'ChristosiPhone6', '0')
+    message.setDeviceBlockStatus('0', 'Jeffreys-iPhone', testBlockStatus)
+    message.setDeviceBlockStatus('1', 'LyonsDesktopXPS-2', blockStatus)
+    message.setDeviceBlockStatus('2', 'Chriss-iPhone', blockStatus)
+    message.setDeviceBlockStatus('3', 'ChristosiPhone6', blockStatus)
+    message.setDeviceBlockStatus('4', 'Justines-iPhone', blockStatus)
     
     payload = message.getPayload()
     
     cgiApplyUrl = routerUrl + "apply.cgi"
-    response = requests.post(cgiApplyUrl, auth=('admin', 'FakePassword'), data=payload)
+    response = requests.post(cgiApplyUrl, auth=('admin', password), data=payload)
     print(response.status_code)
     #print(response.text)
     
